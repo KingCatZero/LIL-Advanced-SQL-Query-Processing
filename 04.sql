2a3f8ca95aa29d8658a300ca29da7a91e70411e1
KingCatZero <39886878+KingCatZero@users.noreply.github.com>
2024-04-04 20:45:06 +0100

    Update 04.sql

--- 04.sql
+++ 04.sql
@@ -1,1 +1,14 @@
-
+SELECT
+	an1.Species
+	,an1.Breed
+	,an1.Name AS Male
+	,an2.Name AS Female
+FROM [dbo].[Animals] AS an1
+	INNER JOIN [dbo].[Animals] AS an2 ON
+		an2.Species = an1.Species
+		AND an2.Breed = an1.Breed
+		AND an2.Gender = 'F'
+		AND an1.Gender = 'M'
+ORDER BY
+	an1.Species
+	,an1.Breed
