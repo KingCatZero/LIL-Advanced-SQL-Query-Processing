7e619a12416682ffd968ab7b65ebee6331c5e07e
KingCatZero <39886878+KingCatZero@users.noreply.github.com>
2024-04-04 20:29:32 +0100

    Update 03.sql

--- 03.sql
+++ 03.sql
@@ -1,1 +1,24 @@
+SELECT
+	*
+FROM (
+	SELECT
+		Species
+		,Breed
+	FROM [dbo].[Animals]
 
+		EXCEPT
+
+	SELECT
+		Species
+		,Breed
+	FROM [dbo].[Animals] AS an2
+	WHERE
+		EXISTS (
+			SELECT TOP 1
+				1
+			FROM [dbo].[Adoptions]
+			WHERE
+				Name = an2.Name
+				AND Species = an2.Species
+		)
+) AS t
