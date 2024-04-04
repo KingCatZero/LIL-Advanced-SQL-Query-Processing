f1739a2a6eea6fd928066f22c0ed8ef58c8650e1
KingCatZero <39886878+KingCatZero@users.noreply.github.com>
2024-04-04 21:09:26 +0100

    Update 05.sql

--- 05.sql
+++ 05.sql
@@ -1,1 +1,30 @@
-
+WITH
+	cteVaccinations AS (
+		SELECT
+			v.Species
+			,v.Vaccination_Time
+			,YEAR(v.Vaccination_Time) AS Vaccination_Year
+			,CONCAT(p.First_Name, ' ', p.Last_Name) AS Staff_Member
+		FROM [dbo].[Vaccinations] AS v
+			INNER JOIN [dbo].[Persons] AS p ON
+				p.Email = v.Email
+	)
+SELECT
+	ISNULL(CAST(Vaccination_Year AS VARCHAR(4)), '*') AS Vaccination_Year
+	,ISNULL(Species, '*') AS Species
+	,ISNULL(Staff_Member, '*') AS Staff_Member
+	,COUNT(1) AS Vaccination_Count
+	,MAX(Vaccination_Year) AS Max_Vaccination_Year
+FROM cteVaccinations
+GROUP BY GROUPING SETS (
+	Vaccination_Year
+	,Species
+	,Staff_Member
+	,(Vaccination_Year, Species)
+	,(Staff_Member, Species)
+	,()
+)
+ORDER BY
+	Vaccination_Year
+	,Species
+	,Staff_Member
